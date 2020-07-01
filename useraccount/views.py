a1161936eaf74e2e184a055a48fe04c594415745
raman9514 <raman9514@gmail.com>
2020-07-01 14:38:37 +0530

    some errors solved

--- useraccount/views.py
+++ useraccount/views.py
@@ -4,47 +4,52 @@ from .forms import SignUpForm,LoginForm
 from django.contrib import messages
 from django.contrib.auth import authenticate , login
 from django.contrib.auth.models import auth
+from django.shortcuts import HttpResponse
 # Create your views here.
 
 
 
 def signup(request):
-    if request.method=='POST':
-        fm=SignUpForm(request.POST)
+    if not request.user.is_authenticated:    
+        if request.method=='POST':
+            fm=SignUpForm(request.POST)
 
-        if fm.is_valid():
-            #fm.save()
-            messages.success(request, 'Account Created Success Fully')
-           
-    if request.method=='GET':
-        fm=SignUpForm()
-    return render(request,'register.html',{"reg_form":fm})    
+            if fm.is_valid():
+                fm.save()
+                messages.success(request, 'Account Created Success Fully')
 
+        if request.method=='GET':
+            fm=SignUpForm()
+        return render(request,'register.html',{"reg_form":fm})    
+    else:
+        return HttpResponse("Your Are Already logedin You cant access this page")
 
 
 
 def login(request):
-    if request.method=='POST':
-        fm=LoginForm(request.POST)
-        
-        
-        username=request.POST['username']
-        password=request.POST['password']
-        user = authenticate(request,username=username , password = password)
-        print(user)
-        if user is not None:
-            print("login success");
-            auth.login(request , user)
-            return redirect('index')
-        else:
-            print("login faild")
-            messages.error(request,'Invalid Details')
-        
+    
+    if not request.user.is_authenticated:
+        if request.method=='POST':
+            fm=LoginForm(request.POST)
 
-    if request.method == 'GET':
-        fm=LoginForm
-    return render(request,'login.html',{'login_form':fm})  
+            username=request.POST['username']
+            password=request.POST['password']
+            user = authenticate(request,username=username , password = password)
+            print(user)
+            if user is not None:
+                print("login success");
+                auth.login(request , user)
+                return redirect('index')
+            else:
+                print("login faild")
+                messages.error(request,'Invalid Details')
 
+
+        if request.method == 'GET':
+            fm=LoginForm
+        return render(request,'login.html',{'login_form':fm})  
+    else:
+        return HttpResponse("You are not required ths action")
 
 def logout(request):
     auth.logout(request)
