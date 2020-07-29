adc7a4e12223e4d9692250f12b22522f777da932
raman9514 <raman9514@gmail.com>
2020-07-29 17:16:58 +0530

    Now Delivery staff can accept order

--- delivery_staff/urls.py
+++ delivery_staff/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
    path('index',views.deliverystaff,name='deliverystaff_index'),
    path('view_request/<int:order_id>',views.view_request,name='view_request'),
+   path('accept_order/<int:order_id>',views.accept_order,name='accept_order'),
 ]
--- delivery_staff/views.py
+++ delivery_staff/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from order.models import Order
 from order.forms import OrderForm
+from django.contrib import messages
 # Create your views here.
 
 def deliverystaff(request):
@@ -20,6 +21,19 @@ def view_request(request,order_id):
         obj = Order.objects.get(id = order_id)
         fm=OrderForm(instance=obj)
         return render(request,'view_request.html',{'form':fm})
+
+    else:
+        return redirect('index')   
+    
+
+
+def accept_order(request,order_id):
+    if request.user.is_authenticated and request.user.is_staff==True:
+        
+        obj = Order.objects.filter(id = order_id)
+        obj.update(order_cancelled=False,order_pending=False,order_picked=True,order_delivered=False) 
+        messages.success(request, 'Order Accepted')
+        return redirect('deliverystaff_index')
 
     else:
         return redirect('index')   
